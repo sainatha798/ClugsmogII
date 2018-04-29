39c6a1376e0db8229a5b3f01d2be7ce871f03555
Sai Natha Reddy Buchupalli <sainatha798@gmail.com>
2018-04-29 17:47:02 +0530

    plotting datapts and centers

--- tester.py
+++ tester.py
@@ -1,6 +1,6 @@
 from numpied import *
 from check import *
-
+import matplotlib.pyplot as plt
 f = open('data_3','r')
 l=[]
 for i in f.readlines():
@@ -8,6 +8,10 @@ for i in f.readlines():
 #print(l[:6])
 #a=Cluster(l,6,20,0.007)
 #a.initialise()
+x= [p[0] for p in l]
+y=[p[1] for p in l]
+plt.scatter(x,y)
+plt.show()
 b=Test(l,6,15,0.01)
 b.initialise()
 print(set(b.labels))
@@ -19,4 +23,9 @@ while(i<5):
 	print(set(b.labels))
 	i+=1
 print(len(b.labels))
+print(b.centers)
+x=[b.idx_to_datapt[i][0] for i in b.centers ]
+y=[b.idx_to_datapt[i][1] for i in b.centers]
+plt.scatter(x,y)
+plt.show()
 #print(b.gravity)
